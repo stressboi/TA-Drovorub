d2df9f4fc1300f7e993658fcf1f4c57e76f104a4
James Brodsky <jbrodsky@splunk.com>
2020-08-14 11:49:28 -0600

    Added a check for UEFI secure boot and renamed field names

--- bin/drovoprobe.sh
+++ bin/drovoprobe.sh
@@ -2,13 +2,23 @@
 # Drovorub checker 
 # brodsky@splunk.com to output more friendly for Splunk UF Scripted Input
 # wraps detection probe from https://media.defense.gov/2020/Aug/13/2002476465/-1/-1/0/CSA_DROVORUB_RUSSIAN_GRU_MALWARE_AUG_2020.PDF into Splunk friendly format
-VERSION=0.1-SplunkUF
+# also checks to see if we can find evidence of Secure Boot
+VERSION=0.2-SplunkUF
 
 # set a date format so we can output a date for each line
 DATETIME=$(date '+%Y-%m-%d %H:%M:%S %Z')
 
 # what's the IP address of the machine?
 IPADD=$(hostname -I)
+
+# see if we can find evidence of EFI secureboot
+
+if [ -d /sys/firmware/efi ]  
+then
+	SECBOOTSTATUS=1
+else
+	SECBOOTSTATUS=0
+fi
 
 # do the probing
 rm -rf testfile
@@ -19,8 +29,8 @@ echo “ASDFZXCV:hf:testfile” > /dev/zero
 sleep 2
 if [ -e testfile ]
 then
-	echo "$DATETIME src_ip=$IPADD drovorub_status='Rootkit likely not present' drovorub_binary_status=0"
+	echo "$DATETIME src_ip=$IPADD finding='Rootkit likely not present' drovorub_status=0 efi_secureboot_status=$SECBOOTSTATUS"
 else
-	echo "$DATETIME src_ip=$IPADD drovorub_status='Rootkit potentially present based on known signature!' drovorub_binary_status=1"
+	echo "$DATETIME src_ip=$IPADD finding='Rootkit potentially present based on known signature!' drovorub_status=1 efi_secureboot_status=$SECBOOTSTATUS"
 fi
 
